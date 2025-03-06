20acee049537158f032fdda14a191afca9c130fe
ramrk399 <ramavtarchauhan2000@gmail.com>
2025-03-06 12:37:40 +0530

    Update play.py

--- SYSTUM/utils/inline/play.py
+++ SYSTUM/utils/inline/play.py
@@ -33,23 +33,23 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "─▷─────────"
+        bar = "─😘─────────"
     elif 10 < umm < 20:
-        bar = "──▷────────"
+        bar = "──🔥────────"
     elif 20 <= umm < 30:
-        bar = "───▷───────"
+        bar = "───❤️‍🔥───────"
     elif 30 <= umm < 40:
-        bar = "────▷──────"
+        bar = "────😚──────"
     elif 40 <= umm < 50:
-        bar = "─────▷─────"
+        bar = "─────😘─────"
     elif 50 <= umm < 60:
-        bar = "──────▷────"
+        bar = "──────🥰────"
     elif 60 <= umm < 70:
-        bar = "───────▷───"
+        bar = "───────😍───"
     elif 70 <= umm < 80:
-        bar = "────────▷──"
+        bar = "────────🥳──"
     elif 80 <= umm < 95:
-        bar = "─────────▷─"   
+        bar = "─────────💞─"   
     else:
         bar = "➥꯭꯭𓆰꯭꯭♔꯭꯭ 𝗝꯭ᴀ꯭፝֟͠ɴ꯭ɪ꯭꯭⎯꯭꯭ꨄ꯭.꯭𝆭 ⃪꯭꯭꯭꯭꯭꯭🔥꯭➛꯭💌꯭꯭"
         
@@ -69,10 +69,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="http://t.me/RAM_HACKER_RP",
+                text="𝗢𝘄𝗻𝗲𝗿", url="http://t.me/RAM_HACKER_RP",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/VIP_RAM_HACKER_PR",
+                text="𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url="https://t.me/VIP_RAM_HACKER_PR",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -91,10 +91,10 @@ def stream_markup(_, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="http://t.me/RAM_HACKER_RP",
+                text="𝗢𝘄𝗻𝗲𝗿", url="http://t.me/RAM_HACKER_RP",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/VIP_RAM_HACKER_PR",
+                text="𝗦𝘂𝗽𝗽𝗼𝗿𝘁", url="https://t.me/VIP_RAM_HACKER_PR",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
